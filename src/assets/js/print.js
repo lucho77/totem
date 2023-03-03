ef81488c95dd83179e12c412a38be2becadf8d2e
lucho77 <luoeonio@gmail.com>
2023-03-03 12:34:12 -0300

    nuevos cambios

--- src/assets/js/print.js
+++ src/assets/js/print.js
@@ -1,7 +1,7 @@
 function print(texto) {
 
     var flexiplelist = texto.split(";")
-    var body = '<div style="font-size:15px; font-family: Arial"><b>'+ flexiplelist[0]+ '</b></div><body><div style="font-size:15px; font-family: Arial">'+flexiplelist[2]+ '</div><div style="font-size:12px; font-family: Arial">Espere ser atendido por el número</div><div style="font-size:60px; font-family: Arial">' +flexiplelist[1] +'</div><div style="font-size:12px; font-family: Arial">Gracias por su visita</div>    </body>';
+    var body = '<div style="font-size:12px; font-family: Arial"><b>'+ flexiplelist[0]+ '</b></div><body><div style="font-size:12px; font-family: Arial">'+flexiplelist[2]+ '</div><div style="font-size:10px; font-family: Arial">Espere ser atendido por el número</div><div style="font-size:30px; font-family: Arial">' +flexiplelist[1] +'</div><div style="font-size:12px; font-family: Arial">Gracias por su visita</div>    </body>';
     var newWin = document.getElementById('print-iframe').contentWindow;
     newWin.document.write(body);
     newWin.document.close(); //important!
